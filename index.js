6908ac849d700e55989317b147cc0a08429c8f38
TheSale <thepavic@gmail.com>
2023-12-07 21:32:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-const inputOperacija = document.getElementById("operacija")
-//const spanResult = document.getElementById("result")
 const buttonIzracunaj = document.getElementById("izracunaj")
-if(inputOperacija.value == " + ") {
+
+function resenje() {
+    const inputOperacija = document.getElementById("operacija")
     const inputX = document.getElementById("x")
     const inputY = document.getElementById("y")
     const spanResult = document.getElementById("result")
-    const result = Number(inputX.value) + Number(inputY.value)
-    spanResult.innerText = result
-} else if(inputOperacija.value == " - " ) {
-    const inputX = document.getElementById("x")
-    const inputY = document.getElementById("y")
-    const spanResult = document.getElementById("result")
-    const result = Number(inputX.value) - Number(inputY.value)
-    spanResult.innerText = result
-} else if(inputOperacija.value == " * ") {
-    const inputX = document.getElementById("x")
-    const inputY = document.getElementById("y")
-    const spanResult = document.getElementById("result")
-    const result = Number(inputX.value) * Number(inputY.value)
-    spanResult.innerText = result
-} else if(inputOperacija.value == " / ") {
-    const inputX = document.getElementById("x")
-    const inputY = document.getElementById("y")
-    const spanResult = document.getElementById("result")
-    const result = Number(inputX.value) / Number(inputY.value)
-    spanResult.innerText = result
-}else {
-    const result = "Morate uneti +, -, * ili /"
-    spanResult.innerText = result
+
+    if (inputOperacija.value == "+") {
+        const result = Number(inputX.value) + Number(inputY.value)
+        spanResult.innerText = result
+    } else if (inputOperacija.value == "-") {
+        const result = Number(inputX.value) - Number(inputY.value)
+        spanResult.innerText = result
+    } else if (inputOperacija.value == "*") {
+        const result = Number(inputX.value) * Number(inputY.value)
+        spanResult.innerText = result
+    } else if (inputOperacija.value == "/") {
+        const result = Number(inputX.value) / Number(inputY.value)
+        spanResult.innerText = result
+    } else {
+        const result = "Morate uneti +, -, * ili /"
+        spanResult.innerText = result
+    }
 }
-buttonIzracunaj.onclick = spanResult
+
+buttonIzracunaj.onclick = resenje
